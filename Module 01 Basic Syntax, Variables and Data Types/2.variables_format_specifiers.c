c91f284dc4b9af1df44c4ab4eca7293d5ac28915
Sniekdho Shafiq <136834563+sniekdhoshafiq@users.noreply.github.com>
2024-12-08 14:14:38 +0600

    Add files via upload

---
+++ Module 01 Basic Syntax, Variables and Data Types/2.variables_format_specifiers.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main()
+{
+    int rahim = 100, karim = 200;
+    printf("%d %d \n", rahim, karim);
+
+    float oil = 2.5;
+    printf("%0.2f \n", oil);
+
+    char ch = 'X';
+    printf("%c \n", ch);
+
+    double dl = 5.4623157894;
+    printf("%lf \n", dl);
+
+    char name[] = "shafiq";
+    printf("%s \n", name);
+
+    return 0;
+}
